51e17efd00a052bc4d4a1dd854f95944bdbcfe84
Esmeralda Robledo <esrobledo.c02@ucamp.utel.edu.mx>
2022-02-06 11:47:54 -0800

    proxy

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -47,7 +47,7 @@ export default function ContactMe(props) {
             };
            
             setBool(true);
-            const res = await axios.post(`/contact`, data);
+            const res = await axios.post(`https://serverportafolio.herokuapp.com/contact`, data);
             if (name.length === 0 || email.length === 0 || message.length === 0) {
                 setBanner(res.data.msg);
                 toast.error(res.data.msg);
